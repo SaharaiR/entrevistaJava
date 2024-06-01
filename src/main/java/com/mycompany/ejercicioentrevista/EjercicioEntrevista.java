3fe1809eccb3bfc3073babf6bbb009d2fc6a0874
Sahara <sahararod@gmail.com>
2024-05-31 18:25:37 -0600

    ejercicio para entrevista de numeros palindromos

---
+++ src/main/java/com/mycompany/ejercicioentrevista/EjercicioEntrevista.java
@@ -0,0 +1,23 @@
+package com.mycompany.ejercicioentrevista;
+
+public class EjercicioEntrevista {
+    
+    public void esPalindrome(int numero){
+        String numeroCadena = String.valueOf(numero);
+        StringBuilder numeroInvertido = new StringBuilder(numeroCadena);
+        //numeroInvertido.reverse();
+        System.out.println(numeroCadena);
+        String cadenaInvertida = numeroInvertido.reverse().toString();
+        System.out.println(cadenaInvertida);
+        if( numeroCadena.equals(cadenaInvertida))
+            System.out.println("Es palindrome");
+        else
+            System.out.println("No es palindrome");
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        EjercicioEntrevista mandarNumero = new EjercicioEntrevista();
+        mandarNumero.esPalindrome(121);
+    }
+}
