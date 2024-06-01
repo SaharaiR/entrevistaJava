b00c979353d69052f595fc2912fb3dd8093da862
Sahara <138946813+SaharaiR@users.noreply.github.com>
2024-05-31 18:28:12 -0600

    Update EjercicioEntrevista.java

--- src/main/java/com/mycompany/ejercicioentrevista/EjercicioEntrevista.java
+++ src/main/java/com/mycompany/ejercicioentrevista/EjercicioEntrevista.java
@@ -16,7 +16,6 @@ public class EjercicioEntrevista {
     }
     
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         EjercicioEntrevista mandarNumero = new EjercicioEntrevista();
         mandarNumero.esPalindrome(121);
     }
